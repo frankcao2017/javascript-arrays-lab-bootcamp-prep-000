2b8581c64d649bf541e2d9da841cc2cdd6ec8ca0
frankcao@me.com Cao <frankcao@me.com>
2018-06-16 17:30:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-const app = "I don't do much."
+var kittens = ["Milo","Otis","Garfield"];
+function destructivelyAppendKitten() {
+  kittens.push("Ralph");
+  return kittens
+}
+
+function destructivelyPrependKitten() {
+  kittens.unshift("Bob");
+  return kittens
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop()
+  return kittens
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift()
+  return kittens
+}
+
+function appendKitten() {
+  var newArr = [...kittens,"Broom"]
+  return newArr
+}
+
+function prependKitten() {
+  var newArrs = ["Arnold",...kittens]
+  return newArrs
+}
+
+function removeLastKitten() {
+  var newArrs1 = kittens.pop()
+  return kittens
+}
